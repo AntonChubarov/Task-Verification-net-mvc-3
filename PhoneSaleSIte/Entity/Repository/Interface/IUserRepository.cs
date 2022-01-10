ab439ed88e5362966d58b66ac911ca3200c99a3b
Anton Chubarov <achlp600@gmail.com>
2022-01-10 12:46:34 +0200

    feat: task 3 completed

---
+++ PhoneSaleSIte/Entity/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Models;
+using Entity.Models;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace Entity.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        public Task<User> AddUserToDbAsync(RegisterModel model);
+        public Task<User> FindUserInDbAsync(LoginModel model);
+    }
+}
---
+++ PhoneSaleSIte/PhoneSaleSIte/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Entity.Repository.Implementation;
+using Entity.Repository.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PhoneSaleSite.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddEFModule(this IServiceCollection service)
+        {
+            service.AddTransient<IUserRepository, UserRepository>();
+
+            //service.AddTransient<IPhoneRepository, PhoneRepository>();
+
+            //service.AddTransient<IOrderRepository, OrderRepository>();
+        }
+    }
+}
